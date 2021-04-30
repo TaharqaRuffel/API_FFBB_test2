b2e7722cb2939309d61cbffec647e70cab76c6ff
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-04-30 10:19:48 +0200

    creation de l'item BrandsItem

--- ScrapyDjangoTest/ScrapyDjangoTest/items.py
+++ ScrapyDjangoTest/ScrapyDjangoTest/items.py
@@ -4,9 +4,14 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
-
+from scrapy_djangoitem import DjangoItem
+from products.models import Product
 
 class ScrapydjangotestItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class BrandsItem(DjangoItem):
+   django_model = Product
+   stock = scrapy.Field() # You can still add extra fields
