b3d3e263936592e3f678937698eb3cf779c0f3a1
Peeters-Ruffel <nguimbi.taharqa@hotmail.fr>
2021-05-01 15:05:07 +0200

    ajout de matchItem à items.py

--- tutorial/tutorial/items.py
+++ tutorial/tutorial/items.py
@@ -10,3 +10,13 @@ class TutorialItem(scrapy.Item):
     # define the fields for your item here like:
     name = scrapy.Field()
     pass
+
+class MatchItem(scrapy.Item):
+    championship = scrapy.Field
+    day = scrapy.Field
+    match_date = scrapy.Field
+    home = scrapy.Field
+    visitor = scrapy.Field
+    score_home = scrapy.Field
+    score_visitor = scrapy.Field
+    plan = scrapy.Field
